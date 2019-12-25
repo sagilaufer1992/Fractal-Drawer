66c2a6308d4701e36cdf3164fa1d3a737cd1ffb6
Sagi <sagi.laufer23@gmail.com>
2019-12-25 12:09:32 +0200

    drew line on screen

--- painter.py
+++ painter.py
@@ -5,6 +5,13 @@ pygame.init()
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Fractal Painter')
 
+white = (255,255,255)
+black = (0,0,0)
+
+gameDisplay.fill(white)
+
+pygame.draw.line(gameDisplay, black, (100,200), (100,600),1)
+
 crashed = False
 
 while not crashed:
@@ -12,6 +19,8 @@ while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
+
+    pygame.display.update()
 
 
 
